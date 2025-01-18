e6119705b3382999f71b37fca3bafb5b6eef1f9a
mohammad-sadeq-khodadadei <m.s.khodadadei@gmail.com>
2025-01-18 14:26:25 +0330

    feat: main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
-  {{ serverAddress }}
+  <Section
+    class="max-w-2xl m-auto !p-10 my-10 flex flex-col items-center gap-5"
+  >
+    <h1 class="text-center text-3xl">خوش آمدید.</h1>
+    <NuxtLink
+      :href="pageRoutes.dashboard"
+      class="rounded-xl border border-blue-500 italic p-2 -skew-x-[10deg]"
+    >
+      <BouncingBtn> رفتن به فرم ساز </BouncingBtn>
+    </NuxtLink>
+  </Section>
 </template>
 
 <script setup lang="ts">
