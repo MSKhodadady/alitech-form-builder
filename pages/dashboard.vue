36b6704fa7c4d01993b8d06e9cabedcf9d3d4358
mohammad-sadeq-khodadadei <m.s.khodadadei@gmail.com>
2025-01-21 15:13:58 +0330

    fix: refactor pagination

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -15,14 +15,14 @@
     <Section
       :class="[
         'w-full h-max min-h-[70vh] ',
-        loading || formList.length == 0
+        loading || formListPages.length == 0
           ? 'flex justify-center  items-center'
           : 'flex flex-col items-stretch gap-16 justify-between',
       ]"
     >
       <template v-if="loading"> لطفا منتظر بمانید. </template>
 
-      <div v-else-if="formList.length == 0">
+      <div v-else-if="formListPages.length == 0">
         هیچ فرمی تا به حال ساخته نشده است.
 
         <NuxtLink class="ms-5 text-blue-700" :href="pageRoutes.newForm"
@@ -33,7 +33,7 @@
       <template v-else>
         <div class="grid grid-cols-3 gap-3">
           <FormBuilderCard
-            v-for="i in currentPageData()"
+            v-for="i in currentPageData"
             :key="i.form_id"
             :name="i.form_title"
             :created="secondsToPerDateStr(i.created_at)"
@@ -42,19 +42,22 @@
           />
         </div>
 
-        <div class="flex items-center justify-center gap-8">
+        <div
+          v-if="totalPages > 1"
+          class="flex items-center justify-center gap-8"
+        >
           <FormIconButton
             icon-name="hugeicons:arrow-right-01"
             :class="defaultBorder"
             @click="movePage(page - 1)"
           />
-          <form @submit.prevent="setPage">
+          <form @submit.prevent="movePage(pageField)">
             <FormTextInput
               container-class="w-10"
               type="text"
               class="text-center"
               v-model="pageField"
-              @focusout="setPage"
+              @focusout="movePage(pageField)"
             />
           </form>
           <span>از</span>
@@ -78,72 +81,83 @@ useHead({
   title: "داشبورد",
 });
 
-const formList = ref<{ page: number; forms: Form[] }[]>([]);
+const formsData = ref<FormsResp["data"] | null>(null);
 const page = ref(1);
-const currentPageData = () =>
-  formList.value.find((p) => p.page == page.value)?.forms ?? [];
+//: page text field for paginator
 const pageField = ref(page.value);
+
 const { handleAuthFetch, loading } = useHandleAuthFetch(true);
-const totalPages = ref(0);
+
+onMounted(async () => {
+  const res = await handleAuthFetch<FormsResp>(() => authApiList.getAllForms());
+
+  if (res) {
+    formsData.value = res.data;
+  }
+});
+
+const totalPages = computed(() =>
+  Math.ceil((formsData.value?.totla_quantity ?? 0) / FORMS_PAGE_COUNT)
+);
+
+const formListPages = computed(() => {
+  //: data didn't fetch yet
+  if (formsData.value == null) return [];
+  /**
+   * we have a forms list, a put data in it one by one,
+   * if the forms filled, create a new page with page number.
+   */
+  const dataPaginated = formsData.value.data.reduce<{
+    lastPage: number;
+    forms: Form[];
+    output: { page: number; forms: Form[] }[];
+  }>(
+    (acc, i) => {
+      const { lastPage, forms, output } = acc;
+      const newForms = [...forms, i];
+
+      //: forms filled
+      if (newForms.length == FORMS_PAGE_COUNT) {
+        return {
+          lastPage: lastPage + 1,
+          forms: [] as Form[],
+          output: [...output, { page: lastPage, forms: newForms }],
+        };
+      } else {
+        return {
+          lastPage: lastPage,
+          forms: newForms,
+          output,
+        };
+      }
+    },
+    { lastPage: 1, forms: [], output: [] }
+  );
+
+  /**
+   * If the forms didn't fill in last new page, we must create new page for it.
+   */
+  const createLastPage =
+    dataPaginated.forms.length > 0
+      ? [
+          ...dataPaginated.output,
+          { page: dataPaginated.lastPage, forms: dataPaginated.forms },
+        ]
+      : dataPaginated.output;
+
+  return createLastPage;
+});
+
+const currentPageData = computed(
+  () => formListPages.value.find((i) => i.page == page.value)?.forms ?? []
+);
 
 function movePage(np: number) {
   if (np <= totalPages.value && np > 0) {
     page.value = np;
     pageField.value = np;
-  }
-}
-
-function setPage() {
-  if (pageField.value > totalPages.value || pageField.value < 0) {
-    pageField.value = page.value;
   } else {
-    page.value = pageField.value;
+    pageField.value = page.value;
   }
 }
-
-async function fetchList() {
-  const res = await handleAuthFetch<FormsResp>(() => authApiList.getAllForms());
-
-  if (res) {
-    const { data, totla_quantity } = res.data;
-
-    totalPages.value = Math.ceil(totla_quantity / FORMS_PAGE_COUNT);
-
-    const dataPaginated = data.reduce<{
-      lastPage: number;
-      forms: Form[];
-      output: { page: number; forms: Form[] }[];
-    }>(
-      (acc, i) => {
-        const { lastPage, forms, output } = acc;
-        const newForms = [...forms, i];
-
-        if (newForms.length == FORMS_PAGE_COUNT) {
-          return {
-            lastPage: lastPage + 1,
-            forms: [] as Form[],
-            output: [...output, { page: lastPage, forms: newForms }],
-          };
-        } else {
-          return {
-            lastPage: lastPage,
-            forms: newForms,
-            output,
-          };
-        }
-      },
-      { lastPage: 1, forms: [], output: [] }
-    );
-
-    formList.value =
-      dataPaginated.forms.length > 0
-        ? [
-            ...dataPaginated.output,
-            { page: dataPaginated.lastPage, forms: dataPaginated.forms },
-          ]
-        : dataPaginated.output;
-  }
-}
-
-onMounted(fetchList);
 </script>
